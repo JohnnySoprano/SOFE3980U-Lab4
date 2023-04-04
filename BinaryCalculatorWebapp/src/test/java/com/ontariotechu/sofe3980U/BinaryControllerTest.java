134530931cad9177c79da80034cd61a554410b82
Johnathon Tannous (100707434) <johnathon.tannous@ontariotechu.net>
2023-04-04 15:54:43 -0400

    u break it we fix it
    fixed 2.0

--- BinaryCalculatorWebapp/src/test/java/com/ontariotechu/sofe3980U/BinaryControllerTest.java
+++ BinaryCalculatorWebapp/src/test/java/com/ontariotechu/sofe3980U/BinaryControllerTest.java
@@ -10,7 +10,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
 import org.junit.runner.RunWith;
-
+import static org.junit.Assert.*;
 import org.junit.*;
 import org.junit.runner.*;
 import org.springframework.beans.factory.annotation.*;
